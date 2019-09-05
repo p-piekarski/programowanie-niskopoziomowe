5686f83636dc99cf3f957324e63ffae46027b5e7
Xiben <thexiben@gmail.com>
2019-09-05 13:36:45 +0200

    Fixed zadanie2

--- Egzamin 1/zadanie2.c
+++ Egzamin 1/zadanie2.c
@@ -2,34 +2,43 @@
 #include <stdlib.h>
 
 
-using namespace std;
 
-union liczba{
-	int calkowita;
-	double rzeczywista;
-};
-
-struct samochod{
-	char marka[20];
-	char model[20];
-	int rocznik;
-	liczba pojemnosc;
-};
+/*
+Tablicê a oraz zmienne p,q i r zdefiniowano w ramce z prawej strony. Jaka bêdzie
+zawartoœæ tablicy po wykonaniu podanego tam fragmentu kodu programu?
+*/
 
 int main()
 {
-	samochod renault;
+
+
+
+
+	int a[7] = {1,1,1,1,1,1};
+	int *p = a;
+	int *q = a+1;
+	int *r = a+6;
+	q[1] = 4;
+	p++;
+	*p = 7;
+	p[2] = 2;
+	r--;
+	*r = 3;
+	*a = 5;
 	
-	cout<<"Podaj markê: ";
-	cin>>renault.marka; //odwo³ujemy siê za pomoc¹ operatora "."
+	printf("a[0] %d\n", a[0]);
+	printf("a[1] %d\n", a[1]);
+	printf("a[2] %d\n", a[2]);
+	printf("a[3] %d\n", a[3]);
+	printf("a[4] %d\n", a[4]);
+	printf("a[5] %d\n", a[5]);
 	
-	cout<<"Podaj pojemnosc: ";
-	cin>>renault.pojemnosc.rzeczywista; //odwo³ujemy siê za pomoc¹ operatora "."
-	
-	//wypisanie danych
-	cout<<"Marka: "<<renault.marka<<endl;
-	cout<<"Pojemnoœæ: "<<renault.pojemnosc.rzeczywista<<endl;
-	
-	system("pause");
-	return 0;
+	printf("p %d\n", *p);
+	printf("q %d\n", *q);
+	printf("r %d\n", *r);
+
+
+
+
 }
+
